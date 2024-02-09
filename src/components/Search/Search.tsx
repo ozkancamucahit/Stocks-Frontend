a23d3104cb0768cf8d878a1fd1e2ff65780a7117
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-10 00:18:07 +0300

    v-6 event handlers

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,20 +1,25 @@
 
 
-import React, { useState } from 'react'
+import React, { SyntheticEvent, useState } from 'react'
 
 interface Props {}
 
 const Search : React.FC<Props> = (props: Props) : JSX.Element => {
     const [Search, setSearch] = useState<string>("");
 
-    const onclick = (e: any) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log('btn e :>> ', e);
     }
 
+    
+    const onClick = (e : /*SyntheticEvent*/ React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      console.log('e :>> ', e);
+    }
   return (
     <div>
-        <input value={Search} placeholder='type to search' onChange={(e) => onclick(e)}></input>
+        <input value={Search} placeholder='type to search' onChange={(e) => handleChange(e)}></input>
+        <button onClick={(e) => onClick(e)} title='Click' >TEXT</button>
     </div>
   )
 }
