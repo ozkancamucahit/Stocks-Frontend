94c05db0f53fd4a3cb03bb0ab75c7d72d1bdfc4a
Mücahit Özkanca <mmucahit17@hotmail.com>
2024-02-19 22:32:32 +0300

    logout added to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import logo from "./logo.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../Context/useAuth";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+
+  const {isLoggedIn, user, logout} = useAuth();
+
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -19,15 +23,27 @@ const Navbar = (props: Props) => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <div className="hover:text-darkBlue">Login</div>
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+          <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
           <a
-            href="/"
+            onClick={logout}  
             className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
           >
-            Signup
+            Logout
           </a>
         </div>
+        ) : (
+        <div className="hidden lg:flex items-center space-x-6 text-back">
+          <Link to="/login" className="hover:text-darkBlue">Login</Link>
+          <Link
+            to="/register"
+            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+          >
+            Register
+          </Link>
+        </div>
+        ) }
       </div>
     </nav>
   );
